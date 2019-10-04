ac9566cf5b76d14b55356dfa091c6cb97442bdb3
Alexander Lloyd <a.lloyd.4@bham.ac.uk>
2019-10-04 13:54:02 +0100

    feat(app): add sass & bulma

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 const outputDir = path.join(__dirname, 'build/');
 
 const isProd = process.env.NODE_ENV === 'production';
@@ -11,10 +13,30 @@ module.exports = {
     path: outputDir,
     filename: 'Index.js'
   },
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: [
+        MiniCssExtractPlugin.loader,
+        {
+          loader: 'css-loader'
+        },
+        {
+          loader: 'sass-loader',
+          options: {
+            sourceMap: true
+          }
+        }
+      ]
+    }]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/index.html',
       inject: false
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].bundle.css'
     })
   ],
   devServer: {
