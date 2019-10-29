cf0f1519bcb576227568641446243bcac665a2c8
Alexander Lloyd <axl639@student.bham.ac.uk>
2019-10-29 12:30:36 +0000

    fix(build): dont bundle spec files

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
       {
         test: /\.tsx?$/,
         use: 'ts-loader',
-        exclude: /node_modules/,
+        exclude: /(node_modules|__tests__)/,
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
