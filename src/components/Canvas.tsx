ffb5de47eee63a254fc0f4a9db84f55c676f7aaa
Alexander Lloyd <axl639@student.bham.ac.uk>
2019-11-02 14:25:48 +0000

    feat(canvas): add mouse event handler

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+
+function onMouseWheel(evt: React.WheelEvent): void {
+  console.log(evt.deltaX, evt.deltaY, evt.deltaZ);
+}
 
 export default function Canvas(): JSX.Element {
-  const canvasRef = React.useRef<HTMLCanvasElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
 
-  return (<canvas ref={canvasRef} />);
+  useEffect((): void => {
+    const canvas: HTMLCanvasElement | null = canvasRef.current;
+    if (canvas == null) {
+      return;
+    }
+
+    const ctx: CanvasRenderingContext2D | null = canvas.getContext('2d');
+    if (ctx == null) {
+      return;
+    }
+
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    ctx.lineTo(300, 150);
+    ctx.stroke();
+  });
+
+  return (
+    <canvas
+      ref={canvasRef}
+      width={400}
+      height={300}
+      onWheel={onMouseWheel}/>
+  );
 }
