659ae8e68a526e3ace87e26b36398ca3d4d2eb8c
Alexander Lloyd <axl639@student.bham.ac.uk>
2020-01-02 10:18:25 +0000

    fix(linting): fix linting issues

--- src/__tests__/lib/render.spec.ts
+++ src/__tests__/lib/render.spec.ts
@@ -10,13 +10,13 @@ describe('WireEntityRenderer', () => {
             y1: 20,
             x2: 30,
             y2: 20
-          }
+        };
 
         const renderer = new WireEntityRenderer();
         renderer.render(ctx, entity);
 
         const events = ctx.__getEvents();
-        expect(events).toMatchSnapshot()
+        expect(events).toMatchSnapshot();
         const calls = ctx.__getDrawCalls();
         expect(calls).toMatchSnapshot();
     });
@@ -33,13 +33,13 @@ describe('FunctionEntityRender', () => {
             width: 100,
             height: 100,
             label: 'f'
-        }
+        };
       
         const renderer = new FunctionEntityRenderer();
         renderer.render(ctx, entity);
 
         const events = ctx.__getEvents();
-        expect(events).toMatchSnapshot()
+        expect(events).toMatchSnapshot();
         const calls = ctx.__getDrawCalls();
         expect(calls).toMatchSnapshot();
     });
@@ -47,7 +47,7 @@ describe('FunctionEntityRender', () => {
     it('should throw warning if label is larger than function box', () => {
         const consoleSpy = jest
           .spyOn(console, 'warn')
-          .mockImplementation(() => {})
+          .mockImplementation();
         const canvas: HTMLCanvasElement = document.createElement('canvas');
         const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
 
@@ -57,7 +57,7 @@ describe('FunctionEntityRender', () => {
             width: 10,
             height: 10,
             label: 'super long string that larger than width of box'
-        }
+        };
 
         const renderer = new FunctionEntityRenderer();
         renderer.render(ctx, entity);
