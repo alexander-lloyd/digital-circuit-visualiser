464746f9d09c42f178df9c049d5f9896b5a86a60
Alexander Lloyd <axl639@student.bham.ac.uk>
2020-01-07 14:34:36 +0000

    feat: add jest eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,42 +1,44 @@
-module.exports =  {
-    parser:  '@typescript-eslint/parser',  // Specifies the ESLint parser
-    extends:  [
-      'eslint:recommended',
-      'plugin:jsdoc/recommended',
-      'plugin:@typescript-eslint/eslint-recommended',
-      'plugin:@typescript-eslint/recommended',
-      'plugin:@typescript-eslint/recommended-requiring-type-checking',
-      'plugin:react/recommended',
-    ],
-   parserOptions:  {
-      ecmaVersion:  2018,  // Allows for the parsing of modern ECMAScript features
-      sourceType:  'module',  // Allows for the use of imports
-      ecmaFeatures: {
-          jsx: true
-      },
-      project: [
-        './tsconfig.app.json',
-        './tsconfig.spec.json'
-      ]
+module.exports = {
+  parser: '@typescript-eslint/parser',  // Specifies the ESLint parser
+  extends: [
+    'eslint:recommended',
+    'plugin:jsdoc/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:react/recommended',
+    'plugin:jest/all'
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,  // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module',  // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true
     },
-    plugins: [
-      "@typescript-eslint",
-      "jsdoc",
-      "react-hooks"
-    ],
-    rules:  {
-      // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-      "@typescript-eslint/no-empty-interface": "warn",
-      "@typescript-eslint/semi": ["error"],
-      "jsdoc/no-types": 0,
-      "jsdoc/require-param-type": 0,
-      "jsdoc/require-returns-type": 0,
-      "react-hooks/rules-of-hooks": "error",
-      "react-hooks/exhaustive-deps": "warn"
-    },
-    settings: {
-        react: {
-            version: 'detect'
-        }
+    project: [
+      './tsconfig.app.json',
+      './tsconfig.spec.json'
+    ]
+  },
+  plugins: [
+    "@typescript-eslint",
+    "jest",
+    "jsdoc",
+    "react-hooks"
+  ],
+  rules: {
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    "@typescript-eslint/no-empty-interface": "warn",
+    "@typescript-eslint/semi": ["error"],
+    "jsdoc/no-types": 0,
+    "jsdoc/require-param-type": 0,
+    "jsdoc/require-returns-type": 0,
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn"
+  },
+  settings: {
+    react: {
+      version: 'detect'
     }
-  };
+  }
+};
