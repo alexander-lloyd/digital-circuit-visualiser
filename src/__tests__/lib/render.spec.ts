c776e72c3499a3f06956361f6e6861c2133ad833
Alexander Lloyd <axl639@student.bham.ac.uk>
2020-02-13 20:21:21 +0000

    fix: fic render spec linting

--- src/__tests__/lib/render.spec.ts
+++ src/__tests__/lib/render.spec.ts
@@ -1,67 +1,435 @@
-import { WireEntity, WireEntityRenderer, FunctionEntity, FunctionEntityRenderer } from '../../lib/render';
+import {
+  WireEntity,
+  WireEntityRenderer,
+  FunctionEntity,
+  FunctionEntityRenderer
+} from "../../lib/render";
 
-describe('WireEntityRenderer', () => {
-    it('should match snapshot', () => {
-        const canvas: HTMLCanvasElement = document.createElement('canvas');
-        const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+describe("wire entity renderer", () => {
+  it("should match snapshot", () => {
+    expect.assertions(2);
 
-        const entity: WireEntity = {
-            x1: 20,
-            y1: 20,
-            x2: 30,
-            y2: 20
-        };
+    const canvas: HTMLCanvasElement = document.createElement("canvas");
+    const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 
-        const renderer = new WireEntityRenderer();
-        renderer.render(ctx, entity);
+    const entity: WireEntity = {
+      x1: 20,
+      y1: 20,
+      x2: 30,
+      y2: 20
+    };
 
-        const events = ctx.__getEvents();
-        expect(events).toMatchSnapshot();
-        const calls = ctx.__getDrawCalls();
-        expect(calls).toMatchSnapshot();
-    });
+    const renderer = new WireEntityRenderer();
+    renderer.render(ctx, entity);
+
+    const events = ctx.__getEvents();
+    expect(events).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "props": Object {},
+          "transform": Array [
+            1,
+            0,
+            0,
+            1,
+            0,
+            0,
+          ],
+          "type": "beginPath",
+        },
+        Object {
+          "props": Object {
+            "x": 20,
+            "y": 20,
+          },
+          "transform": Array [
+            1,
+            0,
+            0,
+            1,
+            0,
+            0,
+          ],
+          "type": "moveTo",
+        },
+        Object {
+          "props": Object {
+            "cpx1": 100,
+            "cpx2": -50,
+            "cpy1": 20,
+            "cpy2": 20,
+            "x": 30,
+            "y": 20,
+          },
+          "transform": Array [
+            1,
+            0,
+            0,
+            1,
+            0,
+            0,
+          ],
+          "type": "bezierCurveTo",
+        },
+        Object {
+          "props": Object {
+            "path": Array [
+              Object {
+                "props": Object {},
+                "transform": Array [
+                  1,
+                  0,
+                  0,
+                  1,
+                  0,
+                  0,
+                ],
+                "type": "beginPath",
+              },
+              Object {
+                "props": Object {
+                  "x": 20,
+                  "y": 20,
+                },
+                "transform": Array [
+                  1,
+                  0,
+                  0,
+                  1,
+                  0,
+                  0,
+                ],
+                "type": "moveTo",
+              },
+              Object {
+                "props": Object {
+                  "cpx1": 100,
+                  "cpx2": -50,
+                  "cpy1": 20,
+                  "cpy2": 20,
+                  "x": 30,
+                  "y": 20,
+                },
+                "transform": Array [
+                  1,
+                  0,
+                  0,
+                  1,
+                  0,
+                  0,
+                ],
+                "type": "bezierCurveTo",
+              },
+            ],
+          },
+          "transform": Array [
+            1,
+            0,
+            0,
+            1,
+            0,
+            0,
+          ],
+          "type": "stroke",
+        },
+      ]
+    `);
+    const calls = ctx.__getDrawCalls();
+    expect(calls).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "props": Object {
+            "path": Array [
+              Object {
+                "props": Object {},
+                "transform": Array [
+                  1,
+                  0,
+                  0,
+                  1,
+                  0,
+                  0,
+                ],
+                "type": "beginPath",
+              },
+              Object {
+                "props": Object {
+                  "x": 20,
+                  "y": 20,
+                },
+                "transform": Array [
+                  1,
+                  0,
+                  0,
+                  1,
+                  0,
+                  0,
+                ],
+                "type": "moveTo",
+              },
+              Object {
+                "props": Object {
+                  "cpx1": 100,
+                  "cpx2": -50,
+                  "cpy1": 20,
+                  "cpy2": 20,
+                  "x": 30,
+                  "y": 20,
+                },
+                "transform": Array [
+                  1,
+                  0,
+                  0,
+                  1,
+                  0,
+                  0,
+                ],
+                "type": "bezierCurveTo",
+              },
+            ],
+          },
+          "transform": Array [
+            1,
+            0,
+            0,
+            1,
+            0,
+            0,
+          ],
+          "type": "stroke",
+        },
+      ]
+    `);
+  });
 });
 
-describe('FunctionEntityRender', () => {
-    it('should match snapshot', () => {
-        const canvas: HTMLCanvasElement = document.createElement('canvas');
-        const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+describe("function entity renderer", () => {
+  it("should match snapshot", () => {
+    expect.assertions(2);
 
-        const entity: FunctionEntity = {
-            x1: 20,
-            y1: 20,
-            width: 100,
-            height: 100,
-            label: 'f'
-        };
-      
-        const renderer = new FunctionEntityRenderer();
-        renderer.render(ctx, entity);
+    const canvas: HTMLCanvasElement = document.createElement("canvas");
+    const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 
-        const events = ctx.__getEvents();
-        expect(events).toMatchSnapshot();
-        const calls = ctx.__getDrawCalls();
-        expect(calls).toMatchSnapshot();
-    });
+    const entity: FunctionEntity = {
+      x1: 20,
+      y1: 20,
+      width: 100,
+      height: 100,
+      label: "f"
+    };
 
-    it('should throw warning if label is larger than function box', () => {
-        const consoleSpy = jest
-          .spyOn(console, 'warn')
-          .mockImplementation();
-        const canvas: HTMLCanvasElement = document.createElement('canvas');
-        const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+    const renderer = new FunctionEntityRenderer();
+    renderer.render(ctx, entity);
 
-        const entity: FunctionEntity = {
-            x1: 20,
-            y1: 20,
-            width: 10,
-            height: 10,
-            label: 'super long string that larger than width of box'
-        };
+    const events = ctx.__getEvents();
+    expect(events).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "props": Object {},
+          "transform": Array [
+            1,
+            0,
+            0,
+            1,
+            0,
+            0,
+          ],
+          "type": "beginPath",
+        },
+        Object {
+          "props": Object {
+            "height": 100,
+            "width": 100,
+            "x": 20,
+            "y": 20,
+          },
+          "transform": Array [
+            1,
+            0,
+            0,
+            1,
+            0,
+            0,
+          ],
+          "type": "rect",
+        },
+        Object {
+          "props": Object {
+            "path": Array [
+              Object {
+                "props": Object {},
+                "transform": Array [
+                  1,
+                  0,
+                  0,
+                  1,
+                  0,
+                  0,
+                ],
+                "type": "beginPath",
+              },
+              Object {
+                "props": Object {
+                  "height": 100,
+                  "width": 100,
+                  "x": 20,
+                  "y": 20,
+                },
+                "transform": Array [
+                  1,
+                  0,
+                  0,
+                  1,
+                  0,
+                  0,
+                ],
+                "type": "rect",
+              },
+            ],
+          },
+          "transform": Array [
+            1,
+            0,
+            0,
+            1,
+            0,
+            0,
+          ],
+          "type": "stroke",
+        },
+        Object {
+          "props": Object {
+            "value": "center",
+          },
+          "transform": Array [
+            1,
+            0,
+            0,
+            1,
+            0,
+            0,
+          ],
+          "type": "textAlign",
+        },
+        Object {
+          "props": Object {
+            "maxWidth": null,
+            "text": "f",
+            "x": 70,
+            "y": 70,
+          },
+          "transform": Array [
+            1,
+            0,
+            0,
+            1,
+            0,
+            0,
+          ],
+          "type": "fillText",
+        },
+        Object {
+          "props": Object {
+            "text": "f",
+          },
+          "transform": Array [
+            1,
+            0,
+            0,
+            1,
+            0,
+            0,
+          ],
+          "type": "measureText",
+        },
+      ]
+    `);
+    const calls = ctx.__getDrawCalls();
+    expect(calls).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "props": Object {
+            "path": Array [
+              Object {
+                "props": Object {},
+                "transform": Array [
+                  1,
+                  0,
+                  0,
+                  1,
+                  0,
+                  0,
+                ],
+                "type": "beginPath",
+              },
+              Object {
+                "props": Object {
+                  "height": 100,
+                  "width": 100,
+                  "x": 20,
+                  "y": 20,
+                },
+                "transform": Array [
+                  1,
+                  0,
+                  0,
+                  1,
+                  0,
+                  0,
+                ],
+                "type": "rect",
+              },
+            ],
+          },
+          "transform": Array [
+            1,
+            0,
+            0,
+            1,
+            0,
+            0,
+          ],
+          "type": "stroke",
+        },
+        Object {
+          "props": Object {
+            "maxWidth": null,
+            "text": "f",
+            "x": 70,
+            "y": 70,
+          },
+          "transform": Array [
+            1,
+            0,
+            0,
+            1,
+            0,
+            0,
+          ],
+          "type": "fillText",
+        },
+      ]
+    `);
+  });
 
-        const renderer = new FunctionEntityRenderer();
-        renderer.render(ctx, entity);
+  it("should throw warning if label is larger than function box", () => {
+      expect.assertions(1);
 
-        expect(consoleSpy).toHaveBeenCalled();
-    });
+    const consoleSpy = jest.spyOn(console, "warn").mockImplementation();
+    const canvas: HTMLCanvasElement = document.createElement("canvas");
+    const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+
+    const entity: FunctionEntity = {
+      x1: 20,
+      y1: 20,
+      width: 10,
+      height: 10,
+      label: "super long string that larger than width of box"
+    };
+
+    const renderer = new FunctionEntityRenderer();
+    renderer.render(ctx, entity);
+
+    expect(consoleSpy).toHaveBeenCalledWith(expect.any(String));
+  });
 });
