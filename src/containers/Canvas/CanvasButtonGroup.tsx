41d719779e928d7dfdd64f4137219f7efa26b775
Alexander Lloyd <axl639@student.bham.ac.uk>
2020-02-15 18:36:33 +0000

    fix(lint): add missing semicolon

--- src/containers/Canvas/CanvasButtonGroup.tsx
+++ src/containers/Canvas/CanvasButtonGroup.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCompressArrowsAlt, faDownload } from '@fortawesome/free-solid-svg-icons';
 
 /**
