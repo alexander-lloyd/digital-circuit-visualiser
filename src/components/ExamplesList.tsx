4fb582db26002d0e11b42edb5fa141d9b885194b
Alexander Lloyd <axl639@student.bham.ac.uk>
2020-02-25 14:26:57 +0000

    feat: examples change code input field

--- src/components/ExamplesList.tsx
+++ src/components/ExamplesList.tsx
@@ -1,20 +1,55 @@
-import React from 'react';
+import React, {useRef} from 'react';
+import {connect} from 'react-redux';
 
 import {EXAMPLES} from '../assets/examples';
+import * as actions from '../containers/App/actions';
+import {DispatchFunction} from '../containers/App/types';
+
+/**
+ * Dispatch Props.
+ */
+interface DispatchProps {
+    setSourceCode: (source: string) => void;
+}
 
 /**
  * Examples List Component.
  *
  * @returns Example List Component.
  */
-export default function ExamplesList(): JSX.Element {
+function ExamplesList({setSourceCode}: DispatchProps): JSX.Element {
+    const selectRef = useRef<HTMLSelectElement>(null);
+    const exampleList = Object.entries(EXAMPLES);
+
+    /**
+     * Handle when an element is selected in the Examples List.
+     */
+    function onExampleSelected(): void {
+        const element = selectRef.current;
+        if (!element) {
+            return;
+        }
+
+        const index = element.selectedIndex - 1;
+        const [, metadata] = exampleList[index];
+        const {source} = metadata;
+        setSourceCode(source);
+    }
+
     return (
         <div className="field has-addons">
             <div className="control is-expanded">
                 <div className="select is-fullwidth">
-                    <select name="country">
+                    <select name="examples"
+                            onChange={onExampleSelected}
+                            ref={selectRef}>
+                        <option disabled
+                                selected
+                                value="">
+                            Select an Example
+                        </option>
                         {
-                        Object.entries(EXAMPLES).map(([key, example]) => (
+                        exampleList.map(([key, example]) => (
                             <option key={key}
                                     value={key}>
                                 { example.name }
@@ -24,12 +59,32 @@ export default function ExamplesList(): JSX.Element {
                     </select>
                 </div>
             </div>
-            <div className="control">
-                <button className="button is-primary"
-                        type="submit">
-                    Choose
-                </button>
-            </div>
         </div>
     );
 }
+
+/**
+ * Map State To Props.
+ *
+ * @returns Empty State.
+ */
+function mapStateToProps(): {} {
+    return {};
+}
+
+/**
+ * Map Dispatch to Props.
+ *
+ * @param dispatch Action dispatcher.
+ * @returns Props.
+ */
+function mapDispatchToProps(dispatch: DispatchFunction): DispatchProps {
+    return {
+        setSourceCode: actions.setSourceCode(dispatch)
+    };
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(ExamplesList);
--- src/containers/Canvas/constants.ts
+++ src/containers/Canvas/constants.ts
@@ -2,5 +2,5 @@ export const SCALING_FACTOR = 1.05;
 export const MINIMUM_SCALE = 0.5;
 export const MAXIMUM_SCALE = 5;
 
-export const INITIAL_CODE = 'AND tensor AND';
+export const INITIAL_CODE = 'let x = AND in AND tensor AND;';
 export const INITIAL_SCALE = 1.0;
