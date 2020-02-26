3f17e9781261d00ba7f9165891cf0d92e26cf8c5
Alexander Lloyd <axl639@student.bham.ac.uk>
2020-02-26 15:38:17 +0000

    feat(help-screen): pressing help button triggers action

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import {connect} from 'react-redux';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faQuestionCircle} from '@fortawesome/free-solid-svg-icons';
+
+import * as actions from '../containers/App/actions';
+import {DispatchFunction} from '../containers/App/types';
+
+/**
+ * HeaderProps.
+ */
+interface HeaderProps {
+    showModal: () => void;
+}
 
 /**
  * Header Component.
  *
+ * @param props Header Properties.
  * @returns Header Component.
  */
-export default function Header(): JSX.Element {
+function Header({showModal}: HeaderProps): JSX.Element {
     return (
         <div className="navbar is-link is-fixed-top">
             <div className="navbar-brand">
@@ -19,7 +31,8 @@ export default function Header(): JSX.Element {
             <div className="navbar-end">
                 <div className="navbar-item">
                     <div className="buttons">
-                        <a className="button is-success">
+                        <a className="button is-success"
+                           onClick={showModal}>
                             <span className="icon is-small">
                                 <FontAwesomeIcon icon={faQuestionCircle} />
                             </span>
@@ -30,3 +43,30 @@ export default function Header(): JSX.Element {
         </div>
     );
 }
+
+/**
+ * Map state to props.
+ *
+ * @returns Component Props.
+ */
+function mapStateToProps(): {} {
+    return {};
+}
+
+/**
+ * Map Dispatch to Props.
+ *
+ * @param dispatch Action dispatcher.
+ * @returns Props.
+ */
+function mapDispatchToProps(dispatch: DispatchFunction): HeaderProps {
+    const {modalShowAction} = actions;
+    return {
+        showModal: (): void => dispatch(modalShowAction())
+    };
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Header);
