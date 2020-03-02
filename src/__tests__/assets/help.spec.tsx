37620fd138e09c06ad6e389c664b29aad34a9583
Alexander Lloyd <axl639@student.bham.ac.uk>
2020-03-02 13:45:16 +0000

    chore(lint): fix linting issues

--- src/__tests__/assets/help.spec.tsx
+++ src/__tests__/assets/help.spec.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import {render, screen} from '@testing-library/react';
+import {render} from '@testing-library/react';
 
-import { HelpContentComponent } from '../../assets/help';
+import {HelpContentComponent} from '../../assets/help';
 
 describe('help component', () => {
     it('should render component', () => {
