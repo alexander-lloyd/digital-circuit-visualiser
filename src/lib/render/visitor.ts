f7af301235298a354cb7c4e82e3f7995e1632d2c
Alexander Lloyd <axl639@student.bham.ac.uk>
2020-03-03 15:09:53 +0000

    feat: remove unused code

--- src/lib/render/visitor.ts
+++ src/lib/render/visitor.ts
@@ -60,21 +60,14 @@ export class ASTRenderer extends ASTVisitor<ASTRendererConfig, Entity> {
         let label: LabelFunction;
         const {name} = ast;
         const imageMetaData = images[name];
-        const wires: Wire[] = [];
         let inputPositions: number[] = [];
         let outputPositions: number[] = [];
 
         if (imageMetaData === undefined) {
             label = buildTextLabelFunction(name);
         } else {
-            const {height, width, inputs, outputs} = imageMetaData;
+            const {inputs, outputs} = imageMetaData;
             label = buildTextImageFunction(imageMetaData);
-            wires.push(
-                // TODO: Remove hard coded Origin.
-                ...inputs.map(([x, y]): Wire => [[0, 0], [x / width, y / height]]),
-                ...outputs.map(([x, y]): Wire => [[0, 0], [x / width, y / height]])
-            );
-
             inputPositions = getTerminatorPositions(inputs.length);
             outputPositions = getTerminatorPositions(outputs.length);
         }
