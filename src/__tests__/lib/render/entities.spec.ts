bb371a10dd8ad803913732bc2d0893376c15fc3d
Alexander Lloyd <axl639@student.bham.ac.uk>
2020-03-14 18:10:24 +0000

    feat: add tests for entity wires

--- src/__tests__/lib/render/entities.spec.ts
+++ src/__tests__/lib/render/entities.spec.ts
@@ -4,44 +4,48 @@ import {
     isFunctionEntity,
     isGroupedEntity
 } from '../../../lib/render/entities';
+import {Point} from '../../../lib/render/types';
 
 describe('function entity', () => {
     it('should scale a function entity', () => {
-        expect.assertions(4);
+        expect.assertions(5);
 
-        const entity = new FunctionEntity(5, 6, 8, 10, () => {}, [], [], []);
+        const entity = new FunctionEntity(5, 6, 8, 10, () => {}, [[[0, 0], [1, 1], [2, 2], [3, 3]]], [], []);
         entity.scale(0.5, 2);
         expect(entity.x).toBe(5);
         expect(entity.y).toBe(6);
         expect(entity.width).toBe(4);
         expect(entity.height).toBe(20);
+        expect(entity.wires).toStrictEqual([[[0, 0], [0.5, 2], [1, 4], [1.5, 6]]]);
     });
 
     it('should translate a function entity', () => {
-        expect.assertions(4);
+        expect.assertions(5);
 
-        const entity = new FunctionEntity(5, 6, 8, 10, () => {}, [], [], []);
+        const entity = new FunctionEntity(5, 6, 8, 10, () => {}, [[[0, 0], [1, 1], [2, 2], [3, 3]]], [], []);
         entity.translate(10, -3);
         expect(entity.x).toBe(15);
         expect(entity.y).toBe(3);
         expect(entity.width).toBe(8);
         expect(entity.height).toBe(10);
+        expect(entity.wires).toStrictEqual([[[10, -3], [11, -2], [12, -1], [13, 0]]]);
     });
 });
 
 describe('grouped entity', () => {
     it('should scale a grouped entity', () => {
-        expect.assertions(12);
+        expect.assertions(14);
 
         const left = new FunctionEntity(1, 2, 1, 1, () => {}, [], [], []);
         const right = new FunctionEntity(2, 2, 1, 1, () => {}, [], [], []);
-        const entity = new GroupedEntity('compose', 1.5, 2, 2, 1, [left, right], []);
+        const entity = new GroupedEntity('compose', 1.5, 2, 2, 1, [left, right], [[[0, 0], [1, 1], [2, 2], [3, 3]]]);
 
         entity.scale(0.5, 2);
         expect(entity.x).toBe(1.5);
         expect(entity.y).toBe(2);
         expect(entity.width).toBe(1);
         expect(entity.height).toBe(2);
+        expect(entity.wires).toStrictEqual([[[0, 0], [0.5, 2], [1, 4], [1.5, 6]]]);
 
         expect(left.x).toBe(1);
         expect(left.y).toBe(2);
@@ -52,20 +56,22 @@ describe('grouped entity', () => {
         expect(right.y).toBe(2);
         expect(right.width).toBe(0.5);
         expect(right.height).toBe(2);
+        expect(right.wires).toHaveLength(0);
     });
 
     it('should translate a grouped entity', () => {
-        expect.assertions(12);
+        expect.assertions(14);
 
         const left = new FunctionEntity(1, 2, 1, 1, () => {}, [], [], []);
         const right = new FunctionEntity(2, 2, 1, 1, () => {}, [], [], []);
-        const entity = new GroupedEntity('compose', 1.5, 2, 2, 1, [left, right], []);
+        const entity = new GroupedEntity('compose', 1.5, 2, 2, 1, [left, right], [[[0, 0], [1, 1], [2, 2], [3, 3]]]);
 
         entity.translate(2, 3);
         expect(entity.x).toBe(3.5);
         expect(entity.y).toBe(5);
         expect(entity.width).toBe(2);
         expect(entity.height).toBe(1);
+        expect(entity.wires).toStrictEqual([[[2, 3], [3, 4], [4, 5], [5, 6]]]);
 
         expect(left.x).toBe(3);
         expect(left.y).toBe(5);
@@ -76,6 +82,21 @@ describe('grouped entity', () => {
         expect(right.y).toBe(5);
         expect(right.width).toBe(1);
         expect(right.height).toBe(1);
+        expect(right.wires).toHaveLength(0);
+    });
+
+    it('should return all children inputs and outputs', () => {
+        expect.assertions(2);
+        const leftInputs: Point[] = [[0, 0], [1, 1]];
+        const rightInputs: Point[] = [[0, 0], [1, 1]];
+        const leftOutputs: Point[] = [];
+        const rightOutputs: Point[] = [[1, 2]];
+        const left = new FunctionEntity(1, 2, 1, 1, () => {}, [], leftInputs, leftOutputs);
+        const right = new FunctionEntity(2, 2, 1, 1, () => {}, [], rightInputs, rightOutputs);
+        const entity = new GroupedEntity('compose', 1.5, 2, 2, 1, [left, right], []);
+
+        expect(entity.inputs).toStrictEqual([...leftInputs, ...rightInputs]);
+        expect(entity.outputs).toStrictEqual([...leftOutputs, ...rightOutputs]);
     });
 });
 
